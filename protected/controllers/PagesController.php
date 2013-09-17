e20ab29eabc5cad9637e1dca8be1e43ca1103d9a
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-09-17 20:11:12 +0300

    restruct

--- protected/controllers/PagesController.php
+++ protected/controllers/PagesController.php
@@ -11,6 +11,45 @@ class PagesController extends Controller
 {
     public function actionIndex()
     {
-        $this->render('index');
+        $user = User::model()->findByPk(Yii::app()->user->id);
+
+        $pages = array();
+
+        try {
+            $pages = Yii::app()->facebook->sdk->api(
+                '/' . $user->fb_id . '/accounts?' . http_build_query(array('fields' => 'id', 'limit' => 10))
+            );
+        } catch (FacebookApiException $e) {
+            $pages = $e->getResult();
+        }
+
+        if (isset($pages['data']) and count($pages['data'])) {
+            $showcasesIds = array();
+            foreach ($pages['data'] as $page) {
+                $showcasesIds[] = $page['id'];
+            }
+            //var_dump($showcasesIds);
+
+            $criteria = new CDbCriteria();
+            $criteria->addInCondition("fb_id", $showcasesIds);
+            $showcases = Showcase::model()->findAll($criteria);
+            var_dump($showcases);
+
+        }
+
+
+
+        $shops = $user->shops;
+        $this->render(
+            'index',
+            array(
+                'shops' => $shops
+            )
+        );
+    }
+
+    public function actionGet()
+    {
+        $this->render('get');
     }
 }
