613b44a38488f75201d808a7654722868d131fb2
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-11-01 15:23:32 +0200

    Management

--- protected/modules/management/views/index/index.php
+++ protected/modules/management/views/index/index.php
@@ -1,17 +1,91 @@
 <?php
 /* @var $this DefaultController */
 
-$this->breadcrumbs=array(
-	$this->module->id,
+$this->menu        = array(
+    array(
+        'label' => Yii::t('label', 'users'),
+        'url'   => array('users/index')
+    ),
+    array(
+        'label' => Yii::t('label', 'shops'),
+        'url'   => array('shops/index')
+    ),
+    array(
+        'label' => Yii::t('label', 'products'),
+        'url'   => array('products/index')
+    ),
+    array(
+        'label' => Yii::t('label', 'categories'),
+        'url'   => array('categories/index')
+    ),
 );
-?>
-<h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
+$this->headerTitle = Yii::t('label', 'management');
 
-<p>
-This is the view content for action "<?php echo $this->action->id; ?>".
-The action belongs to the controller "<?php echo get_class($this); ?>"
-in the "<?php echo $this->module->id; ?>" module.
-</p>
-<p>
-You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
-</p>
+$stats = [];
+
+$ends = false;
+for ($offset = -1 * (int)date('w'); floor($offset / (-7)) < 10; $offset -= 7) {
+    $starts = date('Y-m-d', strtotime($offset . ' days'));
+    $week         = date('W', strtotime($starts));
+    $stats[$week] = [
+        'Users'      => 0,
+        'Shops'      => 0,
+        'Products'   => 0,
+        'Categories' => 0
+    ];
+
+    if ($ends) {
+        $conditions = [
+            'condition' => 'created > :starts AND created < :ends',
+            'params'    => [
+                'starts' => $starts,
+                'ends'   => $ends
+            ]
+        ];
+    } else {
+        $conditions = [
+            'condition' => 'created > :starts',
+            'params'    => [
+                'starts' => $starts
+            ]
+        ];
+    }
+
+    $stats[$week]['Users']      = (int)User::model()->count($conditions);
+    $stats[$week]['Shops']      = (int)Shop::model()->count($conditions);
+    $stats[$week]['Products']   = (int)Product::model()->count($conditions);
+    $stats[$week]['Categories'] = (int)Category::model()->count($conditions);
+
+    $ends = $starts;
+}
+
+$grid = [['Week', 'Users', 'Shops', 'Products', 'Categories']];
+
+foreach (array_reverse($stats, true) as $week=>$params) {
+    $grid[] = [(string)$week, $params['Users'], $params['Shops'], $params['Products'], $params['Categories']];
+}
+
+
+Yii::app()->clientScript->registerScriptFile('https://www.google.com/jsapi');
+Yii::app()->clientScript->registerScript(
+    'application_chart',
+    'google.load("visualization", "1", {packages:["corechart"]});
+    google.setOnLoadCallback(drawChart);
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable('.CJSON::encode($grid).');
+
+        var options = {
+          title: "Mercher progress",
+          backgroundColor: "transparent"
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById("chart_div"));
+        chart.draw(data, options);
+      }
+    ',
+    ClientScript::POS_END
+);
+
+echo CHtml::tag('div', ['id' => 'chart_div', 'style' => 'width: 100%; height: 500px;'])
+
+?>
