96c80fe3b740bf665545b8c127987a673682d3e0
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-11-01 15:48:32 +0200

    Management

--- protected/config/urlManager/rules.php
+++ protected/config/urlManager/rules.php
@@ -2,41 +2,38 @@
 
 return array_merge(
     array(
-        'http://tab.<hostname:(mercher.dev|mercher.net)>/'          => 'tab/index/index',
-        [
-            'management/<controller>/<action>',
-            'pattern'       => 'management/<controller:\w+>/<action:\w+>',
-            'defaultParams' => [
-                'controller' => 'index',
-                'action'     => 'index'
-            ]
-        ],
-        ''                                                          => 'index/index',
-        'logout'                                                    => 'index/logout',
-        'contact'                                                   => 'index/contact',
+        'http://tab.<hostname:(mercher.dev|mercher.net)>/'                                => 'tab/index/index',
+        'management'                                                                      => 'management/index/index',
+        'management/<_c:(users|shops|products|categories)>/<id:\d+>/<_a:(update|delete)>' => 'management/<_c>/<_a>',
+        'management/<_c:(users|shops|products|categories)>/<id:\d+>'                      => 'management/<_c>/view',
+        'management/<_c:(users|shops|products|categories)>/<_a:(create|admin)'            => 'management/<_c>/<_a>',
+        'management/<_c:(users|shops|products|categories)>'                               => 'management/<_c>/index',
+        ''                                                                                => 'index/index',
+        'logout'                                                                          => 'index/logout',
+        'contact'                                                                         => 'index/contact',
         //==Shops==
-        'shops'                                                     => 'shops/index',
-        'shops/new'                                                 => 'shops/create',
-        'shops/<shop_id:\d+>'                                       => 'shops/read',
-        'shops/<shop_id:\d+>/settings'                              => 'shops/update',
-        'shops/<shop_id:\d+>/delete'                                => 'shops/delete',
+        'shops'                                                                           => 'shops/index',
+        'shops/new'                                                                       => 'shops/create',
+        'shops/<shop_id:\d+>'                                                             => 'shops/read',
+        'shops/<shop_id:\d+>/settings'                                                    => 'shops/update',
+        'shops/<shop_id:\d+>/delete'                                                      => 'shops/delete',
         //==Categories==
-        'shops/<shop_id:\d+>/categories'                            => 'categories/index',
-        'shops/<shop_id:\d+>/categories/new'                        => 'categories/create',
-        'shops/<shop_id:\d+>/categories/<category_id:\d+>'          => 'categories/read',
-        'shops/<shop_id:\d+>/categories/<category_id:\d+>/settings' => 'categories/update',
-        'shops/<shop_id:\d+>/categories/<category_id:\d+>/delete'   => 'categories/delete',
+        'shops/<shop_id:\d+>/categories'                                                  => 'categories/index',
+        'shops/<shop_id:\d+>/categories/new'                                              => 'categories/create',
+        'shops/<shop_id:\d+>/categories/<category_id:\d+>'                                => 'categories/read',
+        'shops/<shop_id:\d+>/categories/<category_id:\d+>/settings'                       => 'categories/update',
+        'shops/<shop_id:\d+>/categories/<category_id:\d+>/delete'                         => 'categories/delete',
         //==Products==
-        'shops/<shop_id:\d+>/products'                              => 'products/index',
-        'shops/<shop_id:\d+>/products/new'                          => 'products/create',
-        'shops/<shop_id:\d+>/products/<product_id:\d+>'             => 'products/read',
-        'shops/<shop_id:\d+>/products/<product_id:\d+>/settings'    => 'products/update',
-        'shops/<shop_id:\d+>/products/<product_id:\d+>/delete'      => 'products/delete',
+        'shops/<shop_id:\d+>/products'                                                    => 'products/index',
+        'shops/<shop_id:\d+>/products/new'                                                => 'products/create',
+        'shops/<shop_id:\d+>/products/<product_id:\d+>'                                   => 'products/read',
+        'shops/<shop_id:\d+>/products/<product_id:\d+>/settings'                          => 'products/update',
+        'shops/<shop_id:\d+>/products/<product_id:\d+>/delete'                            => 'products/delete',
         //==Orders==
-        'shops/<shop_id:\d+>/orders'                                => 'orders/index',
-        'shops/<shop_id:\d+>/orders/<order_id:\d+>'                 => 'orders/read',
+        'shops/<shop_id:\d+>/orders'                                                      => 'orders/index',
+        'shops/<shop_id:\d+>/orders/<order_id:\d+>'                                       => 'orders/read',
         //==Design==
-        'shops/<shop_id:\d+>/design'                                => 'design/index',
+        'shops/<shop_id:\d+>/design'                                                      => 'design/index',
     ),
     require(dirname(__FILE__) . '/rest_rules.php'),
     require(dirname(__FILE__) . '/rpc_rules.php')
