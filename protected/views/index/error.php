118eef5ef454d49212f4ef4c579b25bd8edd2581
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-11-06 13:01:53 +0200

    error page fix

--- protected/views/index/error.php
+++ protected/views/index/error.php
@@ -2,11 +2,14 @@
 /* @var $this SiteController */
 /* @var $error array */
 
-$this->pageTitle=Yii::app()->name . ' - Error';
+$this->pageTitle = Yii::app()->name . ' - Error';
 
-Yii::app()->controller->headerTitle = 'Error ' . (int) $code;
+Yii::app()->controller->headerTitle = 'Error ' . (int)$code;
 ?>
 
 <div class="container">
-    <?php echo CHtml::encode($message); ?>
+    <div class="alert alert-danger" style="margin: 80px 0;">
+        <strong>Error <?php echo CHtml::encode($code); ?></strong>
+        <?php echo CHtml::encode($message); ?>
+    </div>
 </div>
