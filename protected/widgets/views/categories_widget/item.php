87062623dd0403adc545d9d4fcafbdd485202a96
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-11-11 18:15:45 +0200

    gridView fix

--- protected/widgets/views/categories_widget/item.php
+++ protected/widgets/views/categories_widget/item.php
@@ -4,14 +4,14 @@
 
 echo CHtml::openTag('div', ['class'=>'row']);
 
-echo CHtml::tag('div', ['class'=>'col-lg-1 col-md-1'], $category->id);
-echo CHtml::tag('div', ['class' => 'col-lg-3 col-md-3'], $category->title);
-echo CHtml::tag('div', ['class' => 'col-lg-5 col-md-5'], $category->description);
-echo CHtml::tag('div', ['class' => 'col-lg-1 col-md-1'], $category->is_active ? Yii::t('label', 'active') : Yii::t('label', 'disabled'));
-echo CHtml::tag('div', ['class' => 'col-lg-1 col-md-1'], $category->is_banned ? Yii::t('label', 'yes') : Yii::t('label', 'no'));
+echo CHtml::tag('div', ['class'=>'col-lg-1 col-md-1 gridCell'], $category->id);
+echo CHtml::tag('div', ['class' => 'col-lg-3 col-md-3 gridCell'], $category->title);
+echo CHtml::tag('div', ['class' => 'col-lg-5 col-md-5 gridCell'], $category->description);
+echo CHtml::tag('div', ['class' => 'col-lg-1 col-md-1 gridCell'], $category->is_active ? Yii::t('label', 'active') : Yii::t('label', 'disabled'));
+echo CHtml::tag('div', ['class' => 'col-lg-1 col-md-1 gridCell'], $category->is_banned ? Yii::t('label', 'yes') : Yii::t('label', 'no'));
 echo CHtml::tag(
     'div',
-    ['class' => 'col-lg-1 col-md-1'],
+    ['class' => 'col-lg-1 col-md-1 gridCell'],
     CHtml::link(
         Yii::t('label', 'edit'),
         Yii::app()->urlManager->createUrl(
