e7a7816c2d51393ad9b91fae8e295ed17d0894c9
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-11-13 17:03:14 +0200

    subscriptions

--- protected/modules/management/views/subscriptions/view.php
+++ protected/modules/management/views/subscriptions/view.php
@@ -2,33 +2,62 @@
 /* @var $this SubscriptionController */
 /* @var $model Subscription */
 
-$this->breadcrumbs=array(
-	'Subscriptions'=>array('index'),
-	$model->title,
+$this->breadcrumbs = array(
+    'Subscriptions' => array('index'),
+    $model->title,
 );
 
-$this->menu=array(
-	array('label'=>'List Subscription', 'url'=>array('index')),
-	array('label'=>'Create Subscription', 'url'=>array('create')),
-	array('label'=>'Update Subscription', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Subscription', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Subscription', 'url'=>array('admin')),
+$this->menu = array(
+    array('label' => 'List Subscription', 'url' => array('index')),
+    array('label' => 'Create Subscription', 'url' => array('create')),
+    array('label' => 'Update Subscription', 'url' => array('update', 'id' => $model->id)),
+    array('label'       => 'Delete Subscription',
+          'url'         => '#',
+          'linkOptions' => array('submit'  => array('delete', 'id' => $model->id),
+                                 'confirm' => 'Are you sure you want to delete this item?'
+          )
+    ),
+    array('label' => 'Manage Subscription', 'url' => array('admin')),
 );
 ?>
 
-<h1>View Subscription #<?php echo $model->id; ?></h1>
+    <h1>View Subscription #<?php echo $model->id; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'created',
-		'updated',
-		'title',
-		'description',
-		'image',
-		'price',
-		'trial_duration',
-		'products_count',
-	),
-)); ?>
+<?php $this->widget(
+    'zii.widgets.CDetailView',
+    array(
+        'data'       => $model,
+        'attributes' => array(
+            'id',
+            'created',
+            'updated',
+            'title',
+            'description',
+            'image',
+            'price',
+            'trial_duration',
+            'products_count',
+        ),
+    )
+); ?>
+
+<?php
+echo CHtml::tag('button', ['class' => 'btn btn-success', 'id' => 'testSubscription'], 'Subscribe');
+Yii::app()->clientScript->registerScript(
+    'testSubscription',
+    '
+        $("#testSubscription").click(function(e){
+            var obj = ' . CJSON::encode(
+        [
+            'method'  => 'pay',
+            'action'  => 'create_subscription',
+            'product' => $this->createAbsoluteUrl('/og/subscription/', ['subscription_id' => $model->id])
+        ]
+    ) . ';
+            FB.ui(obj);
+            console.log(e.target);
+        });
+    ',
+    ClientScript::POS_FB
+);
+?>
