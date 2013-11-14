86514894a9ea87ff89d4daff4709ed969eb4b864
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-11-14 18:30:20 +0200

    ga

--- protected/views/layouts/default.php
+++ protected/views/layouts/default.php
@@ -142,23 +142,24 @@ if ($this->headerTitle or count($this->headerButtons) or count($this->headerTabl
 </footer>
 <!-- footer -->
 
-<script type="text/javascript">
+<!-- Google Analytics -->
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-23393444-12']);
-    _gaq.push(['_setDomainName', 'mercher.net']);
-    _gaq.push(['_trackPageview']);
-
-    (function () {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    })();
-
+    ga('create', 'UA-23393444-12');
+    ga('send', 'pageview');
 </script>
+<!-- End Google Analytics -->
 
 </body>
 </html>
