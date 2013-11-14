c5e1f93b39bb3910c55e779ca47f03ec798431d6
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-11-14 18:22:05 +0200

    ga

--- protected/views/layouts/default.php
+++ protected/views/layouts/default.php
@@ -142,5 +142,23 @@ if ($this->headerTitle or count($this->headerButtons) or count($this->headerTabl
 </footer>
 <!-- footer -->
 
+<script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-23393444-12']);
+    _gaq.push(['_setDomainName', 'mercher.net']);
+    _gaq.push(['_trackPageview']);
+
+    (function () {
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+    })();
+
+</script>
+
 </body>
 </html>
