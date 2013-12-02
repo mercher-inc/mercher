374e12cff6fa3f08cf555dfb6589f57face27a5c
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-12-02 11:31:31 +0200

    ga

--- protected/templates/none/src/ga.js
+++ protected/templates/none/src/ga.js
@@ -1,6 +1,6 @@
 define(['google-analytics'], function () {
     if (typeof appConfig.GA != 'undefined' && typeof appConfig.GA.id != 'undefined') {
-        ga('create', appConfig.GA.id);
+        ga('create', appConfig.GA.id, 'auto');
     }
     return ga;
 });
