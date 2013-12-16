1f7ec3bfdf8bacc644206d5e21e9f0e4b99ed18a
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-12-16 13:35:36 +0200

    PP TS

--- protected/views/index/pages/policy.php
+++ protected/views/index/pages/policy.php
@@ -1,7 +1,113 @@
 <?php
 /* @var $this IndexController */
 
-$this->pageTitle=Yii::app()->name . ' - ' . Yii::t('label', 'policy');
-
-Yii::app()->controller->headerTitle = Yii::t('label', 'policy');
+$this->pageTitle = Yii::app()->name . ' - ' . Yii::t('label', 'policy');
 ?>
+
+<div class="container">
+    <div class="page-header">
+        <h1>
+            Privacy Policy
+        </h1>
+    </div>
+    <p>
+        <strong>
+            Few applications are more transparent than Mercher.
+            We aim to respect your privacy and to build a useful product.
+        </strong>
+    </p>
+
+    <p>
+        According to Facebook&reg;,
+        <em>
+            &ldquo;On Facebook&reg;, your name, profile picture, cover photo, gender, networks, username, and User ID
+            are always publicly available, including to apps. Apps also have access to your friends list and any
+            information you choose to make public.&rdquo;
+        </em>
+    </p>
+
+    <p>
+        <strong>
+            We strive to take an EXTRA step to be clear and transparent with our users.
+        </strong>
+    </p>
+
+    <h2>
+        What information Mercher needs to be functional:
+    </h2>
+    <ul>
+        <li>
+            Access to your fan page. (this is logical as given the way Mercher works, users create their sales page on
+            Mercher and then attach it to the Facebook&reg; fan page. Additional information Mercher uses includes:
+            <ul>
+                <li>
+                    Basic Facebook&reg; Information
+                </li>
+                <li>
+                    Facebook&reg; Email
+                </li>
+                <li>
+                    PayPal&trade; Email
+                </li>
+            </ul>
+        </li>
+        <li>
+            Mercher is a B2B platform. We do not collect information regarding the origination source of visitors, their
+            site activities and purchase behavior. We do not collect any information from your customers. Mercher is not
+            responsible for any data collected by organizations using other third-party technology to complete the
+            ordering process.
+        </li>
+    </ul>
+    <h2>
+        How Mercher uses that information:
+    </h2>
+    <ul>
+        <li>
+            We will not share your personal data (e.g. User ID ) with third parties.
+        </li>
+        <li>
+            We do not sell lists containing personal information about website vendors or customers
+        </li>
+        <li>
+            We allow you to Like, Post, and Share Facebook&reg; Actions on your Fan Page
+        </li>
+        <li>
+            We could use the stored information for the following purposes: to contact you, to investigate, prevent or
+            take action regarding illegal activities, suspected fraud, for violations of Mercher’s terms of use or as
+            otherwise required by law, to improve Mercher’s services, to notify you of changes made to the website or
+            new products and services made available on or through the website, to evaluate your needs and customize
+            website content delivered to you according to those needs, to facilitate the processing of any purchases you
+            make through the Site, to send you promotional material from Mercher and our affiliates, and for other
+            legitimate and lawful business purposes.
+        </li>
+        <li>
+            We may, in the future, display some paid advertisement on the website, which may lead you to an external
+            website. We would not responsible for the content of the external website.
+        </li>
+        <li>
+            Though we do not share any of your data, unless it’s permitted by you, in case of legal action we will
+            support official authorities as much as we can in investigation by providing information required.
+        </li>
+    </ul>
+    <h2>
+        Changes to Privacy Policy
+    </h2>
+    <ul>
+        <li>
+            Mercher, may, from time to time, change its privacy practices because of changes in business practices, and
+            relevant and applicable legal or regulatory requirements, - reasons to better serve your needs, or for any
+            other reason at Mercher’s sole discretion. We do not have the resources to inform each user of changes
+            individually, so we encourage you to review this Policy whenever you use our products.
+        </li>
+    </ul>
+    <p>
+        You can always remove the app from your account by going to App Setting on Facebook&reg;, or write to us at
+        <a href="mailto:support@mercher.net">support@mercher.net</a>
+    </p>
+
+    <p>
+        <strong>
+            Happy Merching!
+        </strong>
+    </p>
+</div>
--- protected/views/index/pages/terms.php
+++ protected/views/index/pages/terms.php
@@ -2,6 +2,347 @@
 /* @var $this IndexController */
 
 $this->pageTitle = Yii::app()->name . ' - ' . Yii::t('label', 'terms');
-
-Yii::app()->controller->headerTitle = Yii::t('label', 'terms');
 ?>
+
+<div class="container">
+<div class="page-header">
+    <h1>
+        Terms of Service Agreement
+    </h1>
+</div>
+<p>
+    Mercher is designed to help users effortlessly turn their passions and talents into a business. Mercher (also
+    referred to as “we” in this document”) hopes this can spur innovation, create jobs, bring communities together
+    and ultimately make the world a little better. As with any platform, Mercher, has a set of ground rules for use.
+</p>
+
+<h2>
+    Acceptance of the Terms:
+</h2>
+
+<p>
+    By signing up and using Mercher you acknowledge that you have read, accepted and agreed to be bound by the terms
+    and conditions set forth in this agreement. If you are entering into this Agreement on behalf of a company or
+    other legal entity, you represent that you have the authority to bind such entity to this agreement, in which
+    case the terms “you” or “your” shall refer to such entity. If you do not have such authority or if you do not
+    agree to this Agreement, you may not use this Service. If you are under the age of eighteen (18), you may use
+    Mercher only with the prior informed consent of your parent or guardian. Make sure you review these Terms with
+    your parent or guardian so that you both understand all of your rights and responsibilities. You are responsible
+    for maintaining the confidentiality of your account and are fully and solely responsible for all activities that
+    occur under your account.
+    <strong>
+        As Mercher is a platform integrated into Facebook&reg;, all store contact must follow
+        Facebook&reg; Advertising Guidelines
+        (<a href="https://www.facebook.com/help/399392800124391/">https://www.facebook.com/help/399392800124391/</a>)
+        and may not sell any goods or  services not allowed on Facebook&reg;.
+    </strong>
+</p>
+
+<h2>
+    Changes to the Terms:
+</h2>
+
+<p>
+    Mercher, may, from time to time, change its Terms of Service Agreement because of changes in business practices,
+    and relevant and applicable legal or regulatory requirements, - reasons to better serve your needs, or for any
+    other reason at Mercher’s sole discretion. Mercher expressly reserves the right to make any changes to this
+    agreement, or to the website and its contents, or the service at any time without prior notice to you. We do not
+    have the resources to inform each user of changes individually, so we encourage you to review this agreement
+    whenever you use our products. If any change to this agreement is not acceptable to you, your only remedy is to
+    stop accessing and using the website and service.
+</p>
+
+<h2>
+    About Mercher:
+</h2>
+
+<p>
+    Mercher is a platform that allows users to offer, sell and buy products. As a platform, Mercher does not own or
+    sell the items or services listed on this website, so the actual contract for sale is directly between the
+    sellers and buyers.
+</p>
+
+<p>
+    While we may provide pricing, shipping, and other guidance in our Services, such guidance is solely
+    informational and you may choose not to follow it. Also, while we may help facilitate the resolution of disputes
+    through various programs, Mercher has no control over and does not guarantee the existence, quality, safety or
+    legality of items or services advertised; the truth or accuracy of users' content or listings; the ability of
+    sellers to sell items or services; the ability of buyers to pay for items or services; or that a buyer or seller
+    will actually complete a transaction or return an item.
+</p>
+
+<h2>
+    Abusing Mercher
+</h2>
+
+<p>
+    Without limiting other remedies, we may, limit, suspend, or terminate our services and user accounts, restrict
+    or prohibit access to, and your activities on, our Services, delay or remove hosted content, remove any special
+    status associated with the account, reduce or eliminate any discounts, and take technical and legal steps to
+    keep you from using our Services if:
+</p>
+<ul>
+    <li>
+        we think that you are creating problems or possible legal liabilities;
+    </li>
+    <li>
+        we think that such restrictions will improve the security of the Mercher community or reduce our or another
+        Mercher user's exposure to financial liabilities
+    </li>
+    <li>
+        we think that you are infringing the rights of third parties; broadly, we prohibit anything that is
+        derogatory to somebody else; anything that you didn't create; or anything that is illegal.
+    </li>
+    <li>
+        we think that you are acting inconsistently with the letter or spirit of this Agreement or our policies;
+    </li>
+    <li>
+        we believe you attempt to modify, reverse engineer, decompile, disassemble, or otherwise reduce or attempt
+        to reduce to a human perceivable form any of the software provided by Mercher or used by Mercher in
+        providing the Service.
+    </li>
+    <li>
+        despite our reasonable endeavors, we are unable to verify or authenticate any information you provide to us
+    </li>
+</ul>
+<p>
+    When a buyer or seller issue arises we may consider the user's performance history and the specific
+    circumstances in applying our policies. We may choose to be more lenient with policy enforcement and to do the
+    right thing for both buyers and sellers. Bottom line: try to do good business both as a seller and a buyer and
+    we’ll do our best to support you.
+</p>
+
+<h2>
+    If You Are a Buyer
+</h2>
+
+<p>
+    When buying an item, you agree that:
+</p>
+<ul>
+    <li>
+        You are responsible for reading the full item listing before making a commitment to buy.
+    </li>
+    <li>
+        You enter into a legally binding contract with the seller to purchase an item when you commit to buy an
+        item.
+    </li>
+    <li>
+        Pay attention to the details of the transaction, because your total price may include taxes, fees, and
+        shipping costs, all of which you are responsible for funding.
+    </li>
+    <li>
+        For motor vehicles and real estate, an offer initiates a non-binding transaction representing a buyer's
+        serious expression of interest in buying the seller's item and does not create a formal contract between the
+        buyer and the seller.
+    </li>
+    <li>
+        We do not transfer legal ownership of items or services from the seller to the buyer.
+    </li>
+    <li>
+        All actual payments are made with PayPal&trade; services, therefore all PayPal&trade; Terms and Conditions are
+        applicable.
+    </li>
+</ul>
+
+<h2>
+    If You are a Seller
+</h2>
+
+<p>
+    When listing an item, you agree that:
+</p>
+<ul>
+    <li>
+        You are responsible for the accuracy and content, and quantity of the listing and item offered.
+    </li>
+    <li>
+        To run a great business, sellers work hard to ensure smooth and professional transactions with their buyers.
+        This includes setting buyer expectations and meeting those expectations. Some of the most basic things you
+        can do as a seller are to provide accurate and consistent details about your item and to be clear and
+        specific about the terms and conditions of the sale, including any applicable taxes. You should also make
+        every effort to provide excellent customer service from start to finish, including:
+    </li>
+    <li>
+        Charging reasonable shipping and handling costs and whether you are willing to ship internationally
+    </li>
+    <li>
+        Specifying your handling time and return policy in your listing
+    </li>
+    <li>
+        Responding to buyers' questions promptly
+    </li>
+    <li>
+        Being professional throughout the transaction
+    </li>
+    <li>
+        Making sure the item is delivered to the buyer as described in your listing
+    </li>
+    <li>
+        Frequently reviewing and updating listings to make sure all information &ndash; such as inventory status and
+        item
+        condition &ndash; is accurate and up to date
+    </li>
+</ul>
+
+<h2>
+    For Buyers and Sellers
+</h2>
+<ul>
+    <li>
+        You understand that your Content is public. It can and will be seen by any user on public social networks
+        and any external links to your item.
+    </li>
+    <li>
+        Mercher does not take responsibility for any of the information provided by the seller, does not handle any
+        payment processing, and takes no responsibility on transaction details.
+    </li>
+    <li>
+        You retain all rights to any Content you submit for upload and use in Mercher. You agree that you own or
+        have use rights to use all Content you reference or post in Mercher. In addition, you agree to the
+        following: When you post Content, you are committing that the Content really, truly belongs only to you and
+        that the posting and use of your Content does not violate the privacy rights, publicity rights, copyrights,
+        contract rights, intellectual property rights, or human rights of any other person or entity. We are not
+        responsible if the Content is not really yours or if you infringe on others' rights; you will be responsible
+        if there are any costs associated with ownership and rights issues.
+    </li>
+    <li>
+        If you terminate your application subscription, your Content from the website will not be used outside of
+        the website anymore, but we might still have it stored in backup files or on servers for any legal reason.
+    </li>
+    <li>
+        If you stop using the Mercher application your Content will remain in our website, unless you actively
+        remove your Content.
+    </li>
+    <li>
+        You agree that you will follow all use and ownership terms in Facebook’s&reg; Statement of Rights and
+        Responsibilities. These change periodically and you should review them regularly.
+    </li>
+</ul>
+
+<h2>
+    About the Transaction
+</h2>
+
+<h3>
+    Returns:
+</h3>
+
+<p>
+    You're required to specify your return policy in your listings, even if you don't accept returns.
+</p>
+
+<p>
+    If you accept returns, a buyer can return an item for any reason, including when they change their mind about a
+    purchase &ndash; as long as the return meets the return requirements &ndash; for example, returning an item within
+    your stated time
+    frame, and meeting item condition requirements.
+</p>
+
+<p>
+    We encourage you to accept returns &ndash; it helps increase buyer satisfaction.
+</p>
+<ul>
+    <li>
+        Specify if items or services can be returned. If you accept returns, you must specify:
+    </li>
+    <li>
+        The time period in which a buyer must notify you about a return
+    </li>
+    <li>
+        The restocking fee, if any
+    </li>
+    <li>
+        Who pays for return shipping
+    </li>
+    <li>
+        If there are item conditions required for return, and clearly state those conditions
+    </li>
+    <li>
+        How the refund is issued
+    </li>
+</ul>
+
+<h3>
+    Disputes:
+</h3>
+
+<p>
+    Given Mercher is not involved in the contractual sale between buyer and seller, Mercher is not an arbitrator in any
+    potential conflict resolution. Any disputes should be resolved using the PayPal&trade; Resolution Process, as PayPal&trade;
+    is
+    the primary mechanism for sale consummation on Mercher. Mercher team will make inquiries and may conduct an
+    investigation into disputes, if alerted, but does not have capacity to be the final arbitrator of disputes. Repeat
+    offenses from users will be noted and Mercher could take actions to suspend their accounts. Otherwise, we recommend
+    taking typical internet commerce precautionary measures, including dealing locally and reviewing seller history.
+</p>
+
+<h3>
+    Feedback:
+</h3>
+
+<p>
+    Feedback is important to maintain the flow of good commerce. Mercher provides no forum or assurance as to the
+    integrity or credibility of buyers and sellers. Both parties are advised to give and receive feedback via tools
+    provided by Facebook&reg; and PayPal&trade; services, following their respective terms of service.
+</p>
+
+<p>
+    When you use the Mercher service to pay for items or services, you agree that we may communicate with you
+    electronically regarding any important information about your purchases or your account.
+</p>
+
+<h2>
+    Limitations of Liability and Disclaimer of Warranties
+</h2>
+
+<p>
+    If any provision of this Agreement is held to be invalid, void or for any reason unenforceable, such provision shall
+    be struck out and shall not affect the validity and enforceability of the remaining provisions.
+</p>
+
+<p>
+    We may change or update this Agreement from time to time. Changes take effect when we post them on the Mercher
+    website.
+</p>
+
+<p>
+    No agency, partnership, joint venture, employee-employer or franchiser-franchisee relationship is intended or
+    created by this Agreement.
+</p>
+
+<p>
+    No Warranties. You acknowledge that the products or services you may purchase are sold by private parties, not by
+    Mercher. WE MAKE NO WARRANTIES OF ANY KIND, EXPRESS OR IMPLIED, WITH RESPECT TO ANY PRODUCTS OR SERVICES SOLD ON OR
+    THROUGH MERCHER.
+</p>
+
+<p>
+    Release. You, on behalf of yourself, your company, subsidiaries, affiliates, officers, directors, members, managers,
+    employees, agents, customers and contractors, (collectively “User Parties”) hereby unconditionally and forever
+    release, discharge and agree to indemnify, defend and hold all of the Mercher associates harmless from any and all
+    claims, demands, suits or other proceedings, and all resulting loss, damage, liability, cost and expense (including
+    reasonable attorneys’ fees), whether under contract, warranty, tort (including negligence, strict liability, product
+    liability or other theory) relating to any claim User Parties may now or later have due to or arising out of your
+    access to and use of the content, the website, and services, any violation of this agreement or any violation of any
+    rights of another. We reserve, and you grant to us, the right to assume exclusive defense and control of any matter
+    subject to indemnification by you.
+</p>
+
+<p>
+    Compliance with Laws. In connection with your access to and use of the website and services and that of any person
+    authorized by you to use the service, you are responsible for compliance with all applicable laws, regulations and
+    policies of all relevant jurisdictions. These assurances and commitments by you shall survive termination of this
+    agreement. The Website can be accessed from countries around the world. You agree to comply with all applicable
+    export and import laws and regulations. In particular, you represent that you are not a party identified in any
+    government export exclusion list.
+</p>
+
+<h2>
+    Contact
+</h2>
+
+<p>
+    Please contact us with any questions at <a href="mailto:support@mercher.net">support@mercher.net</a>
+</p>
+</div>
