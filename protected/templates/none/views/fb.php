147ac4e57c2ce6bd7e7dde73fa336215229b1b86
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-12-17 21:01:07 +0200

    fb fix

--- protected/templates/none/views/fb.php
+++ protected/templates/none/views/fb.php
@@ -5,8 +5,15 @@
 ?>
 
 define(['facebook'], function () {
-    FB.init(<?= CJSON::encode([
-        'appId' =>  Yii::app()->facebook->sdk->getAppId()
-    ]) ?>);
+    FB.init(<?=
+        CJSON::encode(
+            [
+                'appId'  => Yii::app()->facebook->sdk->getAppId(),
+                'status' => true,
+                'cookie' => true,
+                'xfbml'  => true,
+            ]
+        )
+    ?>);
     return FB;
 });
