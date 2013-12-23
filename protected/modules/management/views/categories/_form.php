c2e30c987766975a91af02aa91a41316b7e570b4
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-12-23 17:04:30 +0200

    management

--- protected/modules/management/views/categories/_form.php
+++ protected/modules/management/views/categories/_form.php
@@ -6,65 +6,49 @@
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'category-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
-	'enableAjaxValidation'=>false,
-)); ?>
+    <?php $form = $this->beginWidget(
+        'CActiveForm',
+        array(
+            'id'                   => 'category-form',
+            'enableAjaxValidation' => false,
+            'htmlOptions'          => [
+                'class' => 'main-form'
+            ]
+        )
+    ); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<?php echo $form->errorSummary($model); ?>
+    <?php echo $form->errorSummary($model); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'created'); ?>
-		<?php echo $form->textField($model,'created'); ?>
-		<?php echo $form->error($model,'created'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'title'); ?>
+        <?php echo $form->textField($model, 'title', array('size' => 50, 'maxlength' => 50)); ?>
+        <?php echo $form->error($model, 'title'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'updated'); ?>
-		<?php echo $form->textField($model,'updated'); ?>
-		<?php echo $form->error($model,'updated'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'description'); ?>
+        <?php echo $form->textArea($model, 'description', array('rows' => 6, 'cols' => 50)); ?>
+        <?php echo $form->error($model, 'description'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'shop_id'); ?>
-		<?php echo $form->textField($model,'shop_id'); ?>
-		<?php echo $form->error($model,'shop_id'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'is_active'); ?>
+        <?php echo $form->checkBox($model, 'is_active'); ?>
+        <?php echo $form->error($model, 'is_active'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'title'); ?>
-		<?php echo $form->textField($model,'title',array('size'=>50,'maxlength'=>50)); ?>
-		<?php echo $form->error($model,'title'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'is_banned'); ?>
+        <?php echo $form->checkBox($model, 'is_banned'); ?>
+        <?php echo $form->error($model, 'is_banned'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'description'); ?>
-		<?php echo $form->textArea($model,'description',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'description'); ?>
-	</div>
+    <div class="row buttons">
+        <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'is_active'); ?>
-		<?php echo $form->checkBox($model,'is_active'); ?>
-		<?php echo $form->error($model,'is_active'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'is_banned'); ?>
-		<?php echo $form->checkBox($model,'is_banned'); ?>
-		<?php echo $form->error($model,'is_banned'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
+    <?php $this->endWidget(); ?>
 
 </div><!-- form -->
--- protected/modules/management/views/users/_form.php
+++ protected/modules/management/views/users/_form.php
@@ -6,71 +6,49 @@
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'user-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
-	'enableAjaxValidation'=>false,
-)); ?>
+    <?php $form = $this->beginWidget(
+        'CActiveForm',
+        array(
+            'id'                   => 'user-form',
+            'enableAjaxValidation' => false,
+            'htmlOptions'          => [
+                'class' => 'main-form'
+            ]
+        )
+    ); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<?php echo $form->errorSummary($model); ?>
+    <?php echo $form->errorSummary($model); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'created'); ?>
-		<?php echo $form->textField($model,'created'); ?>
-		<?php echo $form->error($model,'created'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'email'); ?>
+        <?php echo $form->textField($model, 'email', array('size' => 60, 'maxlength' => 250)); ?>
+        <?php echo $form->error($model, 'email'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'updated'); ?>
-		<?php echo $form->textField($model,'updated'); ?>
-		<?php echo $form->error($model,'updated'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'first_name'); ?>
+        <?php echo $form->textField($model, 'first_name', array('size' => 50, 'maxlength' => 50)); ?>
+        <?php echo $form->error($model, 'first_name'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'fb_id'); ?>
-		<?php echo $form->textField($model,'fb_id'); ?>
-		<?php echo $form->error($model,'fb_id'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'last_name'); ?>
+        <?php echo $form->textField($model, 'last_name', array('size' => 50, 'maxlength' => 50)); ?>
+        <?php echo $form->error($model, 'last_name'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>250)); ?>
-		<?php echo $form->error($model,'email'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'is_banned'); ?>
+        <?php echo $form->checkBox($model, 'is_banned'); ?>
+        <?php echo $form->error($model, 'is_banned'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'first_name'); ?>
-		<?php echo $form->textField($model,'first_name',array('size'=>50,'maxlength'=>50)); ?>
-		<?php echo $form->error($model,'first_name'); ?>
-	</div>
+    <div class="row buttons">
+        <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'last_name'); ?>
-		<?php echo $form->textField($model,'last_name',array('size'=>50,'maxlength'=>50)); ?>
-		<?php echo $form->error($model,'last_name'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'is_banned'); ?>
-		<?php echo $form->checkBox($model,'is_banned'); ?>
-		<?php echo $form->error($model,'is_banned'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'last_login'); ?>
-		<?php echo $form->textField($model,'last_login'); ?>
-		<?php echo $form->error($model,'last_login'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
+    <?php $this->endWidget(); ?>
 
 </div><!-- form -->
