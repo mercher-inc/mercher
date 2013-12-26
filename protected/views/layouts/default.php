3a7cf25d0ff32c073d6b96ecd1ef0c22e907093c
Dmitry Les <Dmitry.Les@easternpeak.com>
2013-12-26 17:04:06 +0200

    update hero text

--- protected/views/layouts/default.php
+++ protected/views/layouts/default.php
@@ -204,6 +204,38 @@ if ($this->headerTitle or count($this->headerButtons) or count($this->headerTabl
 ?>
 
 <?php if (!Yii::app()->user->isGuest): ?>
+
+<?php
+    Yii::app()->clientScript->registerScript(
+        'autoShowSidebar',
+        '
+            function setCookie(cname,cvalue,exdays)
+            {
+                var d = new Date();
+                d.setTime(d.getTime()+(exdays*24*60*60*1000));
+                var expires = "expires="+d.toGMTString();
+                document.cookie = cname + "=" + cvalue + "; " + expires;
+            }
+            function getCookie(cname)
+            {
+                var name = cname + "=";
+                var ca = document.cookie.split(\';\');
+                for(var i=0; i<ca.length; i++)
+                {
+                    var c = ca[i].trim();
+                    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+                }
+                return "";
+            }
+            if (!getCookie("disableAutoShowSidebar")) {
+                setTimeout(function(){
+                    $("body").addClass("sidebarVisible");
+                },1000);
+            }
+            setCookie("disableAutoShowSidebar", "true", 365);
+        '
+    );
+?>
 <div class="sidebar">
     <?php
     if (count($this->shopsMenu)) {
