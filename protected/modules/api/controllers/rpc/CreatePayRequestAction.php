281cfd3d391c409082247153db4cbbccbdbe21a9
Dmitry Les <dles@bightgrove.com>
2014-05-12 17:44:27 +0300

    sandbox

--- protected/modules/api/controllers/rpc/CreatePayRequestAction.php
+++ protected/modules/api/controllers/rpc/CreatePayRequestAction.php
@@ -105,7 +105,7 @@ class CreatePayRequestAction extends CAction
                 if ($payResponse instanceof PayResponse) {
                     $order->pay_key = $payResponse->payKey;
                 } elseif ($payResponse instanceof PPFaultMessage) {
-					D($payResponse->error[0]);
+					D($payResponse->error[0], 1);
                     throw new CHttpException(500, $payResponse->error[0]['message'], $payResponse->error[0]['errorId']);
                 } else {
                     throw new CHttpException(500);
@@ -128,6 +128,7 @@ class CreatePayRequestAction extends CAction
 //                    print_r($setPaymentOptionsResponse);
 //                    die;
                 } elseif ($setPaymentOptionsResponse instanceof PPFaultMessage) {
+					D($payResponse->error[0], 1);
                     throw new CHttpException(500, $setPaymentOptionsResponse->error[0]['message'], $setPaymentOptionsResponse->error[0]['errorId']);
                 } else {
                     throw new CHttpException(500);
