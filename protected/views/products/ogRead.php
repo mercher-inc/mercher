90b9fc7c2b66941c8255ccc9b5e196bb0e3741b3
Dmitry Les <dmitriy.s.les@gmail.com>
2014-09-14 17:24:41 +0300

    M1-5 Share link - og page

--- protected/views/products/ogRead.php
+++ protected/views/products/ogRead.php
@@ -11,26 +11,14 @@
     <meta property="og:type"                         content="product" />
     <meta property="og:url"                          content="<?php echo Yii::app()->urlManager->createUrl('products/ogRead', ['product_id' => $product->id]) ?>" />
     <meta property="og:title"                        content="<?php echo $product->title ?>" />
-    <meta property="og:image"                        content="https://s-static.ak.fbcdn.net/images/devsite/attachment_blank.png" />
-    <meta property="product:original_price:amount"   content="Sample Original Price: " />
-    <meta property="product:original_price:currency" content="Sample Original Price: " />
-    <meta property="product:pretax_price:amount"     content="Sample Pre-tax Price: " />
-    <meta property="product:pretax_price:currency"   content="Sample Pre-tax Price: " />
-    <meta property="product:price:amount"            content="Sample Price: " />
-    <meta property="product:price:currency"          content="Sample Price: " />
-    <meta property="product:shipping_cost:amount"    content="Sample Shipping Cost: " />
-    <meta property="product:shipping_cost:currency"  content="Sample Shipping Cost: " />
-    <meta property="product:weight:value"            content="Sample Weight: Value" />
-    <meta property="product:weight:units"            content="Sample Weight: Units" />
-    <meta property="product:shipping_weight:value"   content="Sample Shipping Weight: Value" />
-    <meta property="product:shipping_weight:units"   content="Sample Shipping Weight: Units" />
-    <meta property="product:sale_price:amount"       content="Sample Sale Price: " />
-    <meta property="product:sale_price:currency"     content="Sample Sale Price: " />
-    <meta property="product:sale_price_dates:start"  content="Sample Sale Price Dates: Start" />
-    <meta property="product:sale_price_dates:end"    content="Sample Sale Price Dates: End" />
+    <?php if ($product->image) { ?>
+        <meta property="og:image"                        content="<?php echo $product->image->getSize('l') ?>" />
+    <?php } ?>
+    <?php if ($product->price) { ?>
+        <meta property="product:price:amount"            content="<?php echo $product->price ?>" />
+        <meta property="product:price:currency"          content="USD" />
+    <?php } ?>
 </head>
 <body>
-<?php var_dump($product); ?>
-<?php var_dump($product->image); ?>
 </body>
 </html>
