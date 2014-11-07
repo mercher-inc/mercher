3d767fe068ab4907c1fdb3fcaaab059db085c2de
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-07 15:00:27 +0200

    M1-10 Implement export api

--- protected/config/urlManager/export_rules.php
+++ protected/config/urlManager/export_rules.php
@@ -2,6 +2,18 @@
 
 return array(
     array(
+        'export/users/list',
+        'pattern'   => 'api/export/users',
+        'verb'      => 'GET',
+        'urlSuffix' => false,
+    ),
+    array(
+        'export/shops/list',
+        'pattern'   => 'api/export/shops',
+        'verb'      => 'GET',
+        'urlSuffix' => false,
+    ),
+    array(
         'export/products/list',
         'pattern'   => 'api/export/products',
         'verb'      => 'GET',
